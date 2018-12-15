b8733394e30175a21fc573474d5ddb6cde16139c
Beekay165 <b.kershaw165@gmail.com>
2018-12-15 09:37:32 +0000

    content

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,46 @@
+// webpack.config.js
 const path = require("path");
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CleanWebpackPlugin = require("clean-webpack-plugin");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
-
-module.exports = {
-  entry: {
-    app: "./src/index.js"
-    // about: "./src/js/about.js"
-  },
+const pug = {
+  test: /\.pug$/,
+  use: ["html-loader?attrs=false", "pug-html-loader"]
+};
+const config = {
+  entry: "./src/app.js",
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist"
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
   },
   module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ["html-loader?interpolate"]
-      },
-      {
-        test: /\.(jpg|png)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "images/",
-              publicPath: "images/"
-            }
-          }
-        ]
-      }
-    ]
+    rules: [pug]
   },
   plugins: [
-    // new CleanWebpackPlugin(["dist"]),
-    // new ExtractTextPlugin("styles.css"),
-    // new PurgecssPlugin({
-    //   paths: glob.sync(`${PATHS.src}/*`)
-    // }),
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "src/views/application.html",
-      chunks: ["app"]
+      template: "src/views/index.pug",
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: "about.html",
+      template: "src/views/about.pug",
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: "services.html",
+      template: "src/views/services.pug",
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: "events.html",
+      template: "src/views/events.pug",
+      inject: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "src/views/contact.pug",
+      inject: false
     })
-    // new HtmlWebpackPlugin({
-    //   filename: "about.html",
-    //   template: "src/views/about.html",
-    //   chunks: ["about"]
-    // })
   ]
 };
+module.exports = config;
